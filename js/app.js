5b039ac16192d0ef40c2a30901421dcb1f22e7d5
Daniel Machin <DanielTAYG@gmail.com>
2023-09-25 17:03:09 -0400

    added some sections

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 const matrix = document.querySelector('.matrix')
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+
+
 matrix.onmouseover = event => {
     let iterations = 0
 
@@ -7,17 +10,20 @@ matrix.onmouseover = event => {
     event.target.innerText = event.target.innerText.split("")
     .map((letter, index) => {
         if(index < iterations) {
-            if (event.target.dataset.value !== event.target.dataset.parent) {
-                return event.target.dataset.parent[index]
+            if (event.target.dataset.value !== event.target.innerText) {
+                return event.target.dataset.parent[index] 
             }
     
-        }
+        } 
         return letters[Math.floor(Math.random() * 26)]
         }).join("") 
+
         if(iterations >= event.target.dataset.value.length){
             clearInterval(interval)
         }
 
-    iterations += 1 / 3
+    iterations += 1 / 2
+
   }, 50)
 }
+
