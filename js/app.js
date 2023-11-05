eb8bae2380e5dc58e3d0b70ea21561f8c8b8bf5e
Daniel Machin <DanielTAYG@gmail.com>
2023-11-05 09:53:54 -0500

    working on the skill section

--- js/app.js
+++ js/app.js
@@ -51,8 +51,12 @@ const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 const navBar = document.getElementsByClassName('navbar')[0]
 const backToTop = document.getElementsByClassName('top2')[0]
-
-
+const innerContainer = document.getElementsByClassName('inner-container')[0]
+const innerDiv = document.getElementsByClassName('inner')[0]
+const innerDiv2 = document.getElementsByClassName('inner')[1]
+const innerDiv3 = document.getElementsByClassName('inner')[2]
+const innerContainer2 = document.getElementsByClassName('inner-container')[1]
+const innerContainer3 = document.getElementsByClassName('inner-container')[2]
 
 $(document).ready(function() {
     $(window).scroll(function() {
@@ -69,6 +73,16 @@ $(document).ready(function() {
     })
 })
 
+innerDiv.addEventListener('click', () => {
+    innerContainer.classList.toggle('display')
+})
+
+innerDiv2.addEventListener('click', () => {
+    innerContainer2.classList.toggle('display')
+})
+innerDiv3.addEventListener('click', () => {
+    innerContainer3.classList.toggle('display')
+})
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
     navBar.classList.toggle('active')
