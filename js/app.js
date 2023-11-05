95c7d95e658a641c404ec0e02744c509540bacae
Daniel Machin <DanielTAYG@gmail.com>
2023-11-04 21:18:53 -0400

    added fade in back to top button

--- js/app.js
+++ js/app.js
@@ -50,20 +50,37 @@ matrix.onmouseover = event => {
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 const navBar = document.getElementsByClassName('navbar')[0]
+const backToTop = document.getElementsByClassName('top2')[0]
+
+
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 600) { 
+            $('#back-to-top').fadeIn()
+        } else {
+            $('#back-to-top').fadeOut()
+        }
+    });
+
+    $('#back-to-top').click(function() {
+        $('html, body').animate({ scrollTop: 0 }, 800)
+        return false
+    })
+})
 
 toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-  navBar.classList.toggle('active')
-})
-
-navbarLinks.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
     navBar.classList.toggle('active')
-})
-
-const lockScroll = () => {
-    document.body.classList.toggle('lock-scroll')
-}
-
-
-
+    backToTop.classList.toggle('show')
+  })
+  
+  navbarLinks.addEventListener('click', () => {
+      navbarLinks.classList.toggle('active')
+      navBar.classList.toggle('active')
+  })
+  
+  const lockScroll = () => {
+      document.body.classList.toggle('lock-scroll')
+  }
+  
